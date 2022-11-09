95739333d16f17f1bce52f555d95ec9371ac2654
EvyatarHaim1 <evyataros1@gmail.com>
2022-11-09 17:31:25 +0200

    adding wordpair package

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[700]),
         home: Scaffold(
             appBar: AppBar(title: Text('Word Pair Generator')),
-            body: Center(child: Text('Hello World'))));
+            body: Center(child: Text(wordPair.asPascalCase()))));
   }
 }
